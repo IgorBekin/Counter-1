e7307a72beaa773381ee455925264f4741503f40
igorBekin <igor_bekin@mail.ru>
2023-02-14 13:55:33 +0300

    Initial Commit

---
+++ Counter 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter 1
+//
+//  Created by Игорь Бекин on 14.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
